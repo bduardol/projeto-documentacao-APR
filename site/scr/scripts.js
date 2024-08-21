c2f98b7719acbb88c540798e17aaac9e7f0c6145
Bruno Eduardo <102605344+bduardol@users.noreply.github.com>
2024-08-20 22:34:00 -0300

    Att

---
+++ site/scr/scripts.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('btn1').addEventListener('click', function() {
+      window.location.href = 'https://www.exemplo.com/link1';
+    });});
